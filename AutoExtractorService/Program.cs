9053711da1101ceddefcc4f4114318e72a91e323
Hayato <f19870421@gmail.com>
2026-08-12 16:11:26 +0800

    加入 Log

--- AutoExtractorService/Program.cs
+++ AutoExtractorService/Program.cs
@@ -1,14 +1,49 @@
 using AutoExtractorService;
+using Serilog;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddWindowsService(options =>
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+try
 {
-    options.ServiceName = "AutoExtractorService";
-});
+    Log.Information("正在啟動 AutoExtractorService 託管進程...");
+    var builder = Host.CreateApplicationBuilder(args);
 
-builder.Services.Configure<ExtractorOptions>(builder.Configuration.GetSection(ExtractorOptions.Position));
+    builder.Logging.ClearProviders();
+    builder.Services.AddSerilog();
 
-builder.Services.AddHostedService<Worker>();
+    builder.Services.AddWindowsService(options =>
+    {
+        options.ServiceName = "AutoExtractorService";
+    });
 
-var host = builder.Build();
-host.Run();
+    builder.Services.Configure<ExtractorOptions>(builder.Configuration.GetSection(ExtractorOptions.Position));
+
+    builder.Services.AddHostedService<Worker>();
+
+    var host = builder.Build();
+    host.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "AutoExtractorService 託管進程啟動失敗！");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+
+
+
+
+
+
+
